e1768712f0aadfe2ac39ff77e32be163c485520a
ALELAT <aleks167943@yandex.ru>
2020-02-14 21:07:54 +0300

    Готово - rectangle, ellipse Осталось - closefigure

--- figures.py
+++ figures.py
@@ -5,102 +5,150 @@ import sys
 from PySide2.QtWidgets import QApplication, QWidget
 from PySide2.QtGui import QPainter, QBrush
 from PySide2.QtCore import Qt, QPoint
+# from painter import FigureWidget
 
+from abc import abstractmethod
 
 class Figure:
     def __init__(self, x=0, y=0):
-        self.x = x
-        self.y = y
-    
+        self._x = x
+        self._y = y
+
+    @abstractmethod
     def perimeter(self):
-        return 0.0
-    
+        ...
+
+    @abstractmethod
     def square(self):
-        return 0.0
-    
+        ...
+
+    @property
     def width(self):
-        return 0.0
-    
+        return 0
+
+    @property
     def height(self):
-        return 0.0
-    
+        return 0
+
+    @property
+    def x(self):
+        return self._x
+
+    @x.setter
+    def x(self, x):
+        if not isinstance(x, int):
+            raise TypeError
+        self._x = x
+
+    @property
+    def y(self):
+        return self._y
+
+    @y.setter
+    def y(self, y):
+        if not isinstance(y, int):
+            raise TypeError
+        self._y = y
+
 class Rectangle(Figure):
     def __init__(self, x=0, y=0, w=0, h=0):
-        self.__x = x
-        self.__y = y
-        self.w = w
-        self.h = h
-        
-    def x(self):
-        return self.__x
-    
-    def y(self):
-        return self.__y   
-    
-    def perimeter(self):
-        return 2*(self.w+self.h)
-    
-    def square(self):
-        return self.w*self.h
-    
+        # self.__x = x
+        # self.__y = y
+        super().__init__(x, y)
+        self.width = w
+        self.height = h
+
+    @property
     def width(self):
-        return self.w
-    
+        return self._width
+
+    @width.setter
+    def width(self, width):
+        self._width = width
+
+    @property
     def height(self):
-        return self.h
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        # if isinstance(height, int):
+        #     raise TypeError
+        self._height = height
+
+
+    # def perimeter(self):
+    #     return 2*(self.w+self.h)
+    #
+    # def square(self):
+    #     return self.w*self.h
+    #
+    # def width(self):
+    #     return self.w
+    #
+    # def height(self):
+    #     return self.h
 
 # Импортируйте свой файл с фигурами
 
-class FigureWidget(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setWindowTitle('Рисовалка фигур')
-        self.__figures = []
-        
-    def set_figures(self, figures):
-        self.__figures = figures
-        
-    def paintEvent(self, event):
-        
-        painter = QPainter(self)
-        reset_brush = painter.brush()
-        
-        for figure in self.__figures:
-            if not isinstance(figure, Figure):
-                continue
-            
-            if isinstance(figure, Rectangle):
-                painter.setBrush(QBrush(Qt.red))
-                painter.drawRect(figure.x(), figure.y(), figure.width(), figure.height())
-                continue
-            
-            if isinstance(figure, Ellipse):
-                painter.setBrush(QBrush(Qt.green))
-                painter.drawEllipse(figure.x(), figure.y(), figure.width(), figure.height())
-                continue	
-                
-            if isinstance(figure, CloseFigure): 
-                painter.setBrush(QBrush(Qt.blue))
-                
-                points = []                
-                for point in figure:
-                    points.append(QPoint(point['x'], point['y']))
-                painter.drawPolygon(points)
-                continue
-						
-	
+class Ellipse(Figure):
+    def __init__(self, x=0, y=0, w=0, h=0):
+        # self.__x = x
+        # self.__y = y
+        super().__init__(x, y)
+        self.width = w
+        self.height = h
 
-                                                     
-if __name__ == '__main__':
+    @property
+    def width(self):
+        return self._width
 
-    os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = './platforms'
-    app = QApplication(sys.argv)
-    figure_widget = FigureWidget()
-	
-	# Создайте список фигур
-    figures = [Rectangle(20, 30, 400, 200), Rectangle(100, 300, 300, 50)]
-	
-    figure_widget.set_figures(figures)
-	
-    figure_widget.show()
-    sys.exit(app.exec_())
+    @width.setter
+    def width(self, width):
+        self._width = width
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, height):
+        # if isinstance(height, int):
+        #     raise TypeError
+        self._height = height
+
+# class CloseFigure(Figure):
+#     def __init__(self, x=0, y=0, w=0, h=0):
+#         # self.__x = x
+#         # self.__y = y
+#         super().__init__(x, y)
+#         self.width = w
+#         self.height = h
+#
+#     @property
+#     def width(self):
+#         return self._width
+#
+#     @width.setter
+#     def width(self, width):
+#         self._width = width
+#
+#     @property
+#     def height(self):
+#         return self._height
+#
+#     @height.setter
+#     def height(self, height):
+#         # if isinstance(height, int):
+#         #     raise TypeError
+#         self._height = height
+
+class CloseFigure(Figure):
+    def __init__(self, *args):
+        self.d = [{'x': x0, 'y': y0},
+                  {'x': x1, 'y': y1}]
+
+
+
+
+
